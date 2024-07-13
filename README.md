0b26cf54fadc140ab431e15478f8a86219c9f0a8
Alex Martynenko <oleksandr.martynenko@zoominsoftware.com>
2024-07-13 20:20:22 +0300

    initial commit

---
+++ README.md
@@ -0,0 +1,23 @@
+# micro-markers-plugin
+
+Mark selection support for Micro text editor
+
+## Instalation
+
+Using the package manager:
+
+```bash
+$ micro --plugin install markers
+```
+
+Or from within Micro (a restart is needed after the instalation):
+
+```
+> plugin install markers
+```
+
+Or manually install the plugin by cloning the repository to your Micro `plug` directory:
+
+```bash
+$ git clone https://github.com/AlexanderMartinKane/micro-markers-plugin.git ~/.config/micro/plug/nelua
+```
---
+++ markers.lua
@@ -0,0 +1,117 @@
+local markSelection = false
+
+function toggleMarkSelection(bp)
+    markSelection = not markSelection
+end
+
+function preCursorUp(bp)
+    if markSelection then
+        bp:SelectUp()
+        return false
+    end
+    return true
+end
+
+function preCursorDown(bp)
+    if markSelection then
+        bp:SelectDown()
+        return false
+    end
+    return true
+end
+
+function preCursorLeft(bp)
+    if markSelection then
+        bp:SelectLeft()
+        return false
+    end
+    return true
+end
+
+function preCursorRight(bp)
+    if markSelection then
+        bp:SelectRight()
+        return false
+    end
+    return true
+end
+
+function preWordLeft(bp)
+    if markSelection then
+        bp:SelectWordLeft()
+        return false
+    end
+    return true
+end
+
+function preWordRight(bp)
+    if markSelection then
+        bp:SelectWordRight()
+        return false
+    end
+    return true
+end
+
+function preStartOfLine(bp)
+    if markSelection then
+        bp:SelectToStartOfLine()
+        return false
+    end
+    return true
+end
+
+function preStartOfText(bp)
+    if markSelection then
+        bp:SelectToStartOfText()
+        return false
+    end
+    return true
+end
+
+function preStartOfTextToggle(bp)
+    if markSelection then
+        bp:SelectToStartOfTextToggle()
+        return false
+    end
+    return true
+end
+
+function preEndOfLine(bp)
+    if markSelection then
+        bp:SelectToEndOfLine()
+        return false
+    end
+    return true
+end
+
+function preCursorPageUp(bp)
+    if markSelection then
+        bp:SelectPageUp()
+        return false
+    end
+    return true
+end
+
+function preCursorPageDown(bp)
+    if markSelection then
+        bp:SelectPageDown()
+        return false
+    end
+    return true
+end
+
+function preCursorStart(bp)
+    if markSelection then
+        bp:SelectToStart()
+        return false
+    end
+    return true
+end
+
+function preCursorEnd(bp)
+    if markSelection then
+        bp:SelectToEnd()
+        return false
+    end
+    return true
+end
